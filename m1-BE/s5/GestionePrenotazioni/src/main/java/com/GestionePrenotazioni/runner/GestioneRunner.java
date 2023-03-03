7d51282697ad18669e14025a6f355a1a0ae465ef
MarcoBeschi <beschi0697@gmail.com>
2023-03-03 10:31:24 +0100

    Esercizio Settimanale

---
+++ m1-BE/s5/GestionePrenotazioni/src/main/java/com/GestionePrenotazioni/runner/GestioneRunner.java
@@ -0,0 +1,13 @@
+package com.GestionePrenotazioni.runner;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class GestioneRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("run..");
+
+	}
+
+}
